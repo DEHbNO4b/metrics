d48dcd94df0a3fe1d5f3864339189d88102cfebc
DEHbNO4b <85077586+DEHbNO4b@users.noreply.github.com>
2023-07-06 07:37:17 +0000

    fixet error iter1 is complited

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,13 +4,13 @@ import (
 	"net/http"
 
 	"github.com/DEHbNO4b/metrics/internal/handlers"
-	. "github.com/DEHbNO4b/metrics/internal/middlewares"
+	"github.com/DEHbNO4b/metrics/internal/middlewares"
 )
 
 func main() {
 	ms := handlers.NewMemStorage()
 	serv := http.NewServeMux()
-	serv.Handle(`/update/`, Conveyor(http.HandlerFunc(ms.SetMetrics), IsRightRequest, IsPostReq))
+	serv.Handle(`/update/`, middlewares.Conveyor(http.HandlerFunc(ms.SetMetrics), middlewares.IsRightRequest, middlewares.IsPostReq))
 	err := http.ListenAndServe(`localhost:8080`, serv)
 	if err != nil {
 		panic(err)
