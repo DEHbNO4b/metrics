b5edbab994d101127e316b65ddb60f8d59e10022
DEHbNO4b <85077586+DEHbNO4b@users.noreply.github.com>
2023-07-10 14:30:00 +0000

    fixed all errors

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -7,11 +7,15 @@ import (
 	"github.com/DEHbNO4b/metrics/internal/agent"
 )
 
+var repInterval int = 10
+var pInterval int = 2
+var adress string = "localhost:8080"
+
 func main() {
 	var m runtime.MemStats
 
-	go agent.ReadRuntimeMetrics(&m)
-	go agent.PullMetrics(&m)
+	go agent.ReadRuntimeMetrics(&m, pInterval)
+	go agent.PullMetrics(&m, repInterval, adress)
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	wg.Wait()
