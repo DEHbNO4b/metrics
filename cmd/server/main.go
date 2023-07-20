74cf49dbc61b10412d3976a0b44461c50867386b
Teymuraz <dehbno4b@gmail.com>
2023-07-21 00:24:16 +0300

    added logger for saving request info

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,17 +5,25 @@ import (
 
 	"github.com/DEHbNO4b/metrics/internal/data"
 	"github.com/DEHbNO4b/metrics/internal/handlers"
+	logger "github.com/DEHbNO4b/metrics/internal/loger"
+	"github.com/DEHbNO4b/metrics/internal/middleware"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
 
 func main() {
+	if err := logger.Initialize("info"); err != nil {
+		panic(err)
+	}
 	parseFlag()
 	r := chi.NewRouter()
 	ms := data.NewMetStore()
 	mh := handlers.NewMetrics(ms)
+	r.Use(middleware.WithLogging)
 	r.Post(`/update/{type}/{name}/{value}`, http.HandlerFunc(mh.SetMetrics))
 	r.Get(`/value/{type}/{name}`, http.HandlerFunc(mh.GetMetric))
 	r.Get(`/`, mh.GetMetrics)
+	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
 	err := http.ListenAndServe(flagRunAddr, r)
 	if err != nil {
 		panic(err)
