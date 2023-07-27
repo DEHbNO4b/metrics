e4765c63dd757be8f1fa27fdefbb47064ac3cbd0
Teymuraz <dehbno4b@gmail.com>
2023-07-27 22:15:08 +0300

    carryed  metricReader map from agent to data package

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -3,7 +3,6 @@ package agent
 import (
 	"bytes"
 	"encoding/json"
-	"math/rand"
 	"net/http"
 	"runtime"
 	"time"
@@ -12,37 +11,6 @@ import (
 	logger "github.com/DEHbNO4b/metrics/internal/loger"
 	"go.uber.org/zap"
 )
-
-var metricReaders = map[string]func(m *runtime.MemStats) float64{
-	"Alloc":         func(m *runtime.MemStats) float64 { return float64(m.Alloc) },
-	"BuckHashSys":   func(m *runtime.MemStats) float64 { return float64(m.BuckHashSys) },
-	"Frees":         func(m *runtime.MemStats) float64 { return float64(m.Frees) },
-	"GCCPUFraction": func(m *runtime.MemStats) float64 { return float64(m.GCCPUFraction) },
-	"GCSys":         func(m *runtime.MemStats) float64 { return float64(m.GCSys) },
-	"HeapAlloc":     func(m *runtime.MemStats) float64 { return float64(m.HeapAlloc) },
-	"HeapIdle":      func(m *runtime.MemStats) float64 { return float64(m.HeapIdle) },
-	"HeapInuse":     func(m *runtime.MemStats) float64 { return float64(m.HeapInuse) },
-	"HeapObjects":   func(m *runtime.MemStats) float64 { return float64(m.HeapObjects) },
-	"HeapReleased":  func(m *runtime.MemStats) float64 { return float64(m.HeapReleased) },
-	"HeapSys":       func(m *runtime.MemStats) float64 { return float64(m.HeapSys) },
-	"LastGC":        func(m *runtime.MemStats) float64 { return float64(m.LastGC) },
-	"Lookups":       func(m *runtime.MemStats) float64 { return float64(m.Lookups) },
-	"MCacheInuse":   func(m *runtime.MemStats) float64 { return float64(m.MCacheInuse) },
-	"MCacheSys":     func(m *runtime.MemStats) float64 { return float64(m.MCacheSys) },
-	"MSpanInuse":    func(m *runtime.MemStats) float64 { return float64(m.MSpanInuse) },
-	"MSpanSys":      func(m *runtime.MemStats) float64 { return float64(m.MSpanSys) },
-	"Mallocs":       func(m *runtime.MemStats) float64 { return float64(m.Mallocs) },
-	"NextGC":        func(m *runtime.MemStats) float64 { return float64(m.NextGC) },
-	"NumForcedGC":   func(m *runtime.MemStats) float64 { return float64(m.NumForcedGC) },
-	"NumGC":         func(m *runtime.MemStats) float64 { return float64(m.NumGC) },
-	"OtherSys":      func(m *runtime.MemStats) float64 { return float64(m.OtherSys) },
-	"PauseTotalNs":  func(m *runtime.MemStats) float64 { return float64(m.PauseTotalNs) },
-	"StackInuse":    func(m *runtime.MemStats) float64 { return float64(m.StackInuse) },
-	"StackSys":      func(m *runtime.MemStats) float64 { return float64(m.StackSys) },
-	"Sys":           func(m *runtime.MemStats) float64 { return float64(m.Sys) },
-	"TotalAlloc":    func(m *runtime.MemStats) float64 { return float64(m.TotalAlloc) },
-	"RandomValue":   func(m *runtime.MemStats) float64 { return rand.Float64() },
-}
 
 type Agent struct {
 	m      *runtime.MemStats
@@ -71,12 +39,13 @@ func (a Agent) PullMetrics(interval int) {
 	var reportInterval = time.Duration(interval) * time.Second
 	for {
 		for _, el := range a.gauges {
-			reader, ok := metricReaders[el.ID]
-			if !ok {
-				logger.Log.Error("dont have metric with that name", zap.String("name", el.ID))
-			}
-			val := reader(a.m)
-			el.Value = &val
+			el.ReadValue(a.m)
+			// reader, ok := metricReaders[el.ID]
+			// if !ok {
+			// 	logger.Log.Error("dont have metric with that name", zap.String("name", el.ID))
+			// }
+			// val := reader(a.m)
+			// el.Value = &val
 			go a.sendMetric(el)
 		}
 		d := int64(1)
--- internal/data/metrics.go
+++ internal/data/metrics.go
@@ -1,5 +1,13 @@
 package data
 
+import (
+	"math/rand"
+	"runtime"
+
+	logger "github.com/DEHbNO4b/metrics/internal/loger"
+	"go.uber.org/zap"
+)
+
 type Metrics struct {
 	ID    string   `json:"id"`              // имя метрики
 	MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
@@ -12,123 +20,76 @@ func NewMetric() Metrics {
 	var value float64 = 0
 	return Metrics{Delta: &delta, Value: &value}
 }
+func (m *Metrics) ReadValue(mem *runtime.MemStats) {
+	reader, ok := metricReaders[m.ID]
+	if !ok {
+		logger.Log.Error("dont have metric with that name", zap.String("name", m.ID))
+	}
+	val := reader(mem)
+	m.Value = &val
+}
 
+var metricReaders = map[string]func(m *runtime.MemStats) float64{
+	"Alloc":         func(m *runtime.MemStats) float64 { return float64(m.Alloc) },
+	"BuckHashSys":   func(m *runtime.MemStats) float64 { return float64(m.BuckHashSys) },
+	"Frees":         func(m *runtime.MemStats) float64 { return float64(m.Frees) },
+	"GCCPUFraction": func(m *runtime.MemStats) float64 { return float64(m.GCCPUFraction) },
+	"GCSys":         func(m *runtime.MemStats) float64 { return float64(m.GCSys) },
+	"HeapAlloc":     func(m *runtime.MemStats) float64 { return float64(m.HeapAlloc) },
+	"HeapIdle":      func(m *runtime.MemStats) float64 { return float64(m.HeapIdle) },
+	"HeapInuse":     func(m *runtime.MemStats) float64 { return float64(m.HeapInuse) },
+	"HeapObjects":   func(m *runtime.MemStats) float64 { return float64(m.HeapObjects) },
+	"HeapReleased":  func(m *runtime.MemStats) float64 { return float64(m.HeapReleased) },
+	"HeapSys":       func(m *runtime.MemStats) float64 { return float64(m.HeapSys) },
+	"LastGC":        func(m *runtime.MemStats) float64 { return float64(m.LastGC) },
+	"Lookups":       func(m *runtime.MemStats) float64 { return float64(m.Lookups) },
+	"MCacheInuse":   func(m *runtime.MemStats) float64 { return float64(m.MCacheInuse) },
+	"MCacheSys":     func(m *runtime.MemStats) float64 { return float64(m.MCacheSys) },
+	"MSpanInuse":    func(m *runtime.MemStats) float64 { return float64(m.MSpanInuse) },
+	"MSpanSys":      func(m *runtime.MemStats) float64 { return float64(m.MSpanSys) },
+	"Mallocs":       func(m *runtime.MemStats) float64 { return float64(m.Mallocs) },
+	"NextGC":        func(m *runtime.MemStats) float64 { return float64(m.NextGC) },
+	"NumForcedGC":   func(m *runtime.MemStats) float64 { return float64(m.NumForcedGC) },
+	"NumGC":         func(m *runtime.MemStats) float64 { return float64(m.NumGC) },
+	"OtherSys":      func(m *runtime.MemStats) float64 { return float64(m.OtherSys) },
+	"PauseTotalNs":  func(m *runtime.MemStats) float64 { return float64(m.PauseTotalNs) },
+	"StackInuse":    func(m *runtime.MemStats) float64 { return float64(m.StackInuse) },
+	"StackSys":      func(m *runtime.MemStats) float64 { return float64(m.StackSys) },
+	"Sys":           func(m *runtime.MemStats) float64 { return float64(m.Sys) },
+	"TotalAlloc":    func(m *runtime.MemStats) float64 { return float64(m.TotalAlloc) },
+	"RandomValue":   func(m *runtime.MemStats) float64 { return rand.Float64() },
+}
 var gauges = []Metrics{
-	{
-		ID:    "Alloc",
-		MType: "gauge",
-	},
-	{
-		ID:    "BuckHashSys",
-		MType: "gauge",
-	},
-	{
-		ID:    "Frees",
-		MType: "gauge",
-	},
-	{
-		ID:    "GCCPUFraction",
-		MType: "gauge",
-	},
-	{
-		ID:    "GCSys",
-		MType: "gauge",
-	},
-	{
-		ID:    "HeapAlloc",
-		MType: "gauge",
-	},
-	{
-		ID:    "HeapIdle",
-		MType: "gauge",
-	},
-	{
-		ID:    "HeapInuse",
-		MType: "gauge",
-	},
-	{
-		ID:    "HeapObjects",
-		MType: "gauge",
-	},
-	{
-		ID:    "HeapReleased",
-		MType: "gauge",
-	},
-	{
-		ID:    "HeapSys",
-		MType: "gauge",
-	},
-	{
-		ID:    "LastGC",
-		MType: "gauge",
-	},
-	{
-		ID:    "Lookups",
-		MType: "gauge",
-	},
-	{
-		ID:    "MCacheInuse",
-		MType: "gauge",
-	},
-	{
-		ID:    "MCacheSys",
-		MType: "gauge",
-	},
-	{
-		ID:    "MSpanInuse",
-		MType: "gauge",
-	},
-	{
-		ID:    "MSpanSys",
-		MType: "gauge",
-	},
-	{
-		ID:    "Mallocs",
-		MType: "gauge",
-	},
-	{
-		ID:    "NextGC",
-		MType: "gauge",
-	},
-	{
-		ID:    "NumForcedGC",
-		MType: "gauge",
-	},
-	{
-		ID:    "NumGC",
-		MType: "gauge",
-	},
-	{
-		ID:    "OtherSys",
-		MType: "gauge",
-	},
-	{
-		ID:    "PauseTotalNs",
-		MType: "gauge",
-	},
-	{
-		ID:    "StackInuse",
-		MType: "gauge",
-	},
-	{
-		ID:    "StackSys",
-		MType: "gauge",
-	},
-	{
-		ID:    "Sys",
-		MType: "gauge",
-	},
-	{
-		ID:    "TotalAlloc",
-		MType: "gauge",
-	},
-	{
-		ID:    "RandomValue",
-		MType: "gauge",
-	},
+	{ID: "Alloc", MType: "gauge"},
+	{ID: "BuckHashSys", MType: "gauge"},
+	{ID: "Frees", MType: "gauge"},
+	{ID: "GCCPUFraction", MType: "gauge"},
+	{ID: "GCSys", MType: "gauge"},
+	{ID: "HeapAlloc", MType: "gauge"},
+	{ID: "HeapIdle", MType: "gauge"},
+	{ID: "HeapInuse", MType: "gauge"},
+	{ID: "HeapObjects", MType: "gauge"},
+	{ID: "HeapReleased", MType: "gauge"},
+	{ID: "HeapSys", MType: "gauge"},
+	{ID: "LastGC", MType: "gauge"},
+	{ID: "Lookups", MType: "gauge"},
+	{ID: "MCacheInuse", MType: "gauge"},
+	{ID: "MCacheSys", MType: "gauge"},
+	{ID: "MSpanInuse", MType: "gauge"},
+	{ID: "MSpanSys", MType: "gauge"},
+	{ID: "Mallocs", MType: "gauge"},
+	{ID: "NextGC", MType: "gauge"},
+	{ID: "NumForcedGC", MType: "gauge"},
+	{ID: "NumGC", MType: "gauge"},
+	{ID: "OtherSys", MType: "gauge"},
+	{ID: "PauseTotalNs", MType: "gauge"},
+	{ID: "StackInuse", MType: "gauge"},
+	{ID: "StackSys", MType: "gauge"},
+	{ID: "Sys", MType: "gauge"},
+	{ID: "TotalAlloc", MType: "gauge"},
+	{ID: "RandomValue", MType: "gauge"},
 }
 
 func NewGauges() []Metrics {
-
 	return gauges
 }
