c8aca0630f28164ac59695b1d7a22ed23cfb1218
Teymuraz <dehbno4b@gmail.com>
2023-07-31 09:45:34 +0300

    added new flags

--- cmd/agent/flag.go
+++ cmd/agent/flag.go
@@ -6,14 +6,22 @@ import (
 	"strconv"
 )
 
-var endpoint string
-var reportInterval int
-var pollInterval int
+var (
+	endpoint       string
+	reportInterval int
+	pollInterval   int
+	storeInterval  int
+	filepath       string
+	restore        bool
+)
 
 func parseFlag() {
 	flag.StringVar(&endpoint, "a", "localhost:8080", "endpoint adress")
 	flag.IntVar(&reportInterval, "r", 10, "report interval")
 	flag.IntVar(&pollInterval, "p", 2, "poll interval")
+	flag.IntVar(&storeInterval, "i", 300, "store interval")
+	flag.StringVar(&filepath, "f", "/tmp/metrics-db.json", "file storage path")
+	flag.BoolVar(&restore, "r", true, "restore")
 	flag.Parse()
 
 	if ep := os.Getenv("ADDRESS"); ep != "" {
