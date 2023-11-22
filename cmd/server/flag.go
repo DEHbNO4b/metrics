cb438ae866cc2421bde5670c3729b3062d4bf8c3
DEHbNO4b <85077586+DEHbNO4b@users.noreply.github.com>
2023-11-22 14:19:02 +0000

    added reading conf file

--- cmd/server/flag.go
+++ cmd/server/flag.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/DEHbNO4b/metrics/internal/config"
 	logger "github.com/DEHbNO4b/metrics/internal/loger"
@@ -17,7 +21,13 @@ var (
 	cryptoConfPath  string
 	storeInterval   int
 	restore         bool
+	confPath        string
+	configFromFile  string
 )
+
+type cfgFromFile struct {
+	Config map[string]string
+}
 
 func parseFlag() {
 	flag.StringVar(&runAddr, "a", "localhost:8080", "adress and port for running")
@@ -25,6 +35,7 @@ func parseFlag() {
 	flag.StringVar(&cryptoConfPath, "crypto-key", "", "crypto config file path")
 	flag.StringVar(&filestoragepath, "f", "/tmp/metrics-db.json", "file storage path")
 	flag.StringVar(&dsn, "d", "", "dsn for postgres")
+	flag.StringVar(&confPath, "c", "", "path for conf file")
 	flag.IntVar(&storeInterval, "i", 300, "data store interval")
 	flag.BoolVar(&restore, "r", true, "restore_flag")
 	flag.Parse()
@@ -45,6 +56,9 @@ func parseFlag() {
 	if fp := os.Getenv("FILE_STORAGE_PATH"); fp != "" {
 		filestoragepath = fp
 	}
+	if cnf := os.Getenv("CONFIG"); cnf != "" {
+		confPath = cnf
+	}
 	if k := os.Getenv("KEY"); k != "" {
 		key = k
 	}
@@ -64,5 +78,37 @@ func parseFlag() {
 		if err != nil {
 			logger.Log.Error(err.Error())
 		}
+	}
+	if confPath != "" {
+		f, err := os.OpenFile(confPath, os.O_RDONLY, 0755)
+		if err != nil {
+			logger.Log.Error(err.Error())
+			return
+		}
+		defer f.Close()
+
+		sc := bufio.NewScanner(f)
+		var text = ""
+		for sc.Scan() {
+			line := sc.Text()
+			if strings.Contains(line, "//") {
+				str := strings.Split(line, "//")
+				line = str[0] + "\n"
+			}
+			text = text + line
+		}
+		if err := sc.Err(); err != nil {
+			logger.Log.Error(err.Error())
+			return
+		}
+
+		cfg := make(map[string]interface{})
+		err = json.Unmarshal([]byte(text), &cfg)
+		if err != nil {
+			logger.Log.Error(err.Error())
+			return
+		}
+		fmt.Printf("%+v", cfg)
+
 	}
 }
